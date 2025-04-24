8ca45baec122a80c3491b263ad13d5ffe48af3d0
dev-reactbd <dev.reactbd@gmail.com>
2025-04-24 15:31:35 +0600

    next updated

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "/dev-reactbd", // Replace with your GitHub repo name
-  trailingSlash: true,
 };
 
 export default nextConfig;
