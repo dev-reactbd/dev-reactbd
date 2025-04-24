6658204883c15804b371ca7e990d4a918c85f189
Noor Mohammad <noor.jsdivs@gmail.com>
2025-04-24 15:13:01 +0600

    output added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/dev-reactbd", // Replace with your GitHub repo name
+  trailingSlash: true,
 };
 
 export default nextConfig;
