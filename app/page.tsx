378a3f36fa178a46461e2a03c982ef1e42ffddc3
dev-reactbd <dev.reactbd@gmail.com>
2025-04-24 15:37:18 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="p-10 flex items-center justify-center">
+    <div className="p-10 flex flex-col items-center justify-center">
+      <h1 className="text-xl font-semibold underline underline-offset-2 decoration-[1px]">
+        DEV REACTBD
+      </h1>
       <p>Hello from dev.reactbd</p>
+      <p>Working on progress...</p>
     </div>
   );
 }
