df889f338eb18fe704d48505a08d1cd609061a6d
noorjsdivs <noor.jsdivs@gmail.com>
2025-06-11 18:11:11 +0600

    logo updated

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,18 +1,18 @@
-import { Zap } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
 
 const Footer = () => {
   return (
     <footer className="bg-[#001d3d] text-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="flex items-center mb-4 md:mb-0">
-            <div className="w-8 h-8 bg-gradient-to-r from-[#ffc300] to-[#ffd60a] rounded-lg flex items-center justify-center">
-              <Zap className="w-5 h-5 text-[#001d3d]" />
-            </div>
-            <span className="ml-2 text-xl font-bold">ReactBD</span>
-          </div>
+          <Link
+            href={"/"}
+            className="text-reactbdBlack bg-reactbdWhite/90 px-4 py-2 rounded-md hover:bg-reactbdWhite hoverEffect"
+          >
+            <Logo />
+          </Link>
 
           <div className="flex flex-wrap justify-center md:justify-end space-x-6 mb-4 md:mb-0">
             <Link
